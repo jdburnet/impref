c2eedfef45c2e74174104ce2cb71827d95eeeefc
Jackson Burnette <jacksondburnette@gmail.com>
2019-03-29 13:48:46 +0000

    add test declarations for c singly linked list

--- c/slinked-list/slinked-list.h
+++ c/slinked-list/slinked-list.h
@@ -21,3 +21,16 @@ void sll_insert(int value, struct SLinkedList *sll);
 int sll_remove(int key, struct SLinkedList *sll);
 
 struct SLinkedList *init_sll(void);
+
+/* test declarations */
+int test_new_list_is_empty(void);
+
+int test_able_to_insert(void);
+
+int test_remove_from_empty(void);
+
+int test_remove_head(void);
+
+int test_remove_from_body(void);
+
+int test_remove_key_does_not_exist(void);
